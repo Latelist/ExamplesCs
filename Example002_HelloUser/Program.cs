b71d0e5bf2d394455205a513ab0b5c620cb43f0a
Salatchen <pozhivi.s.moe@gmail.com>
2022-06-13 23:38:59 +0300

    Example003 added

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,14 @@
-﻿Console.Write("Введите ваше имя:");
-string username = Console.ReadLine();
+﻿string username = "";
+
+while(true) {
+  Console.Write("Введите ваше имя: ");
+  username = Console.ReadLine();
+  if (username == "") {
+    Console.WriteLine("Тебе сука че непонятно? Сказали же по-русски: введи имя! Давай по новой");
+    continue;
+  }
+  break;
+};
+
 Console.Write("Привет, ");
 Console.WriteLine(username);
